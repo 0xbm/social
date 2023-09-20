7bee099f9b48c7bf4c2c0029e0cd92937400a0d1
0xbm <0xmb@protonmail.com>
2023-09-20 20:53:32 +0200

    create User Profile Model

--- X/models.py
+++ X/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    follows = models.ManyToManyField("self", related_name="followed_by", symetrical=False, blank=True)
+    follows = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
 
