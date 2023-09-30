ba80c72efe443a3745263689df859d763c59fb02
0xbm <0xmb@protonmail.com>
2023-09-30 14:29:53 +0200

    add posts

--- X/views.py
+++ X/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-from .models import Profile
+from .models import Profile, Post
 from django.contrib import messages
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    if request.user.is_authenticated:
+        posts = Post.objects.all().order_by("-created_at")
+    return render(request, 'home.html', {'posts': posts})
 
 
 def profile_list(request):
@@ -19,6 +21,7 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+        posts = Post.objects.filter(user_id=pk).order_by("-created_at")
         if request.method == "POST":
             current_user_profile = request.user.profile
             action = request.POST['follow']
@@ -27,7 +30,7 @@ def profile(request, pk):
             elif action == "follow":
                 current_user_profile.follows.add(profile)
             current_user_profile.save()
-        return render(request, 'profile.html', {'profile': profile})
+        return render(request, 'profile.html', {'profile': profile, "posts": posts})
     else:
         messages.success(request, ("You Must Be Logged In !!!"))
         return redirect('home')
