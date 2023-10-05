5545666dc4df2fb8effc211fb1eb1a7f1d6af846
0xbm <0xmb@protonmail.com>
2023-10-05 10:16:49 +0200

    add picture form

--- X/forms.py
+++ X/forms.py
@@ -1,7 +1,15 @@
 from django import forms
-from .models import Post
+from .models import Post, Profile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+
+class ProfilePicForm(forms.ModelForm):
+    profile_image = forms.ImageField(label="Profile Picture")
+
+    class Meta:
+        model = Profile
+        fields = ('profile_image',)
 
 
 class PostForm(forms.ModelForm):
