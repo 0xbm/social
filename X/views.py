e30cf8bc2cfd1c5653f6deaac3dad31fa5416a56
0xbm <0xmb@protonmail.com>
2023-10-29 12:27:35 +0100

    add search users

--- X/views.py
+++ X/views.py
@@ -3,8 +3,8 @@ from .models import Profile, Post
 from django.contrib import messages
 from .forms import PostForm, SignUpForm, ProfilePicForm
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
-from django import forms
+# from django.contrib.auth.forms import UserCreationForm
+# from django import forms
 from django.contrib.auth.models import User
 
 
@@ -229,3 +229,12 @@ def search(request):
         return render(request, "search.html", {'search': search, 'searched': searched})
     else:
         return render(request, "search.html", {})
+
+
+def search_user(request):
+    if request.method == "POST":
+        search = request.POST['search']
+        searched = User.objects.filter(username__contains=search)
+        return render(request, "search_user.html", {'search': search, 'searched': searched})
+    else:
+        return render(request, "search_user.html", {})
